446a72d198ba741030402065ecbd5c7536b89ac4
chondon_kumar <chondon914@gmail.com>
2025-03-29 01:50:12 +0600

    add asynchandler

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import connectDB from "./Db/index.js";
 import dotenv from "dotenv"
-
+import app from "./app.js"
 dotenv.config();
 
-connectDB();
+const port = process.env.port || 3000;
+
+connectDB()
+.then(() => {
+    console.log("Database connection successfull");
+    app.on ("error", (err) => {
+        console.log("Mongodb can not connect to server", err)
+    })
+    app.listen(port,() => {
+        console.log(`Server is running on port : ${port}`)
+    })
+})
+.catch((err) => {
+    console.log("Database connection faied", err)
+})
